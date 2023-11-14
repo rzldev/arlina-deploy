9d801121412a119f57248efb048e6b4712c93bc2
rzldev <amrizalrf@gmail.com>
2023-11-14 19:27:56 +0700

    Update export data sh scripts

--- export-data.sh
+++ export-data.sh
@@ -3,6 +3,9 @@
 source .env
 mkdir -p mysql
 
+DATE=$(date +%Y_%m_%d_%H_%M)
+FILE=mysql/arlinadb-data-$DATE.sql
+
 mysqldump --host=127.0.0.1 --port=$MYSQL_PORT -u $MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE | \
 sed 's/^CREATE TABLE /CREATE TABLE IF NOT EXISTS /' | \
-sed 's/^DROP TABLE IF EXISTS /-- DROP TABLE IF EXISTS /' > mysql/arlinadb-data.sql
+sed 's/^DROP TABLE IF EXISTS /-- DROP TABLE IF EXISTS /' > $FILE
--- export-schema.sh
+++ export-schema.sh
@@ -3,6 +3,9 @@
 source .env
 mkdir -p mysql
 
+DATE=$(date +%Y_%m_%d_%H_%M)
+FILE=mysql/arlinadb-schema-$DATE.sql
+
 mysqldump --host=127.0.0.1 --port=$MYSQL_PORT -u $MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE --no-data | \
 sed 's/^CREATE TABLE /CREATE TABLE IF NOT EXISTS /' | \
-sed 's/^DROP TABLE IF EXISTS /-- DROP TABLE IF EXISTS /' > mysql/arlinadb-schema.sql
+sed 's/^DROP TABLE IF EXISTS /-- DROP TABLE IF EXISTS /' > $FILE
