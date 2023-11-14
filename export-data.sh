2f5deeff93e4af886cf6fc8c347ed050755d3d9b
rzldev <amrizalrf@gmail.com>
2023-11-14 20:52:07 +0700

    Update mysql scripts

--- export-data.sh
+++ export-data.sh
@@ -9,3 +9,5 @@ FILE=mysql/arlinadb-data-$DATE.sql
 mysqldump --host=127.0.0.1 --port=$MYSQL_PORT -u $MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE | \
 sed 's/^CREATE TABLE /CREATE TABLE IF NOT EXISTS /' | \
 sed 's/^DROP TABLE IF EXISTS /-- DROP TABLE IF EXISTS /' > $FILE
+
+echo "Mysql data successfully exported with path $FILE"
--- export-schema.sh
+++ export-schema.sh
@@ -9,3 +9,5 @@ FILE=mysql/arlinadb-schema-$DATE.sql
 mysqldump --host=127.0.0.1 --port=$MYSQL_PORT -u $MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE --no-data | \
 sed 's/^CREATE TABLE /CREATE TABLE IF NOT EXISTS /' | \
 sed 's/^DROP TABLE IF EXISTS /-- DROP TABLE IF EXISTS /' > $FILE
+
+echo "Mysql schema successfully exported with path $FILE"
