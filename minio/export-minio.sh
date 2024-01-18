3ba7b2119ce71d45ef4bd2d935b66a3a43e91d64
rzldev <amrizalrf@gmail.com>
2024-01-18 23:39:12 +0700

    Restructuring

--- minio/export-minio.sh
+++ minio/export-minio.sh
@@ -2,7 +2,7 @@
 
 CONTAINER_NAME=arlina-minio
 DATE=$(date +%Y_%m_%d_%H_%M)
-FILE=./minio/arlina-bucket-$DATE.tar.gz
+FILE=./arlina-bucket-$DATE.tar.gz
 
 docker exec -it $CONTAINER_NAME sh -c "cd /data && tar -zxvf arlina-bucket.tar.gz arlina-bucket"
 docker cp $CONTAINER_NAME:/data/arlina-bucket.tar.gz $FILE
--- mysql/export-schema.sh
+++ mysql/export-schema.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 source .env
-mkdir -p mysql
 
 DATE=$(date +%Y_%m_%d_%H_%M)
-FILE=mysql/arlinadb-schema-$DATE.sql
+FILE=arlinadb-schema-$DATE.sql
 
 mysqldump --host=127.0.0.1 --port=$MYSQL_PORT -u $MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE --no-data | \
 sed 's/^CREATE TABLE /CREATE TABLE IF NOT EXISTS /' | \
