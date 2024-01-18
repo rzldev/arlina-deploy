73267d92f722c9daee21dd0c5d14e8daa17c0733
rzldev <amrizalrf@gmail.com>
2024-01-18 23:35:38 +0700

    New init sql files

---
+++ mysql/init-company.sql
@@ -0,0 +1,2 @@
+INSERT INTO company_profile (name, description, email, phoneNumber, address, created_at, updated_at)
+VALUES ('Arlina', '', '', '', '', now(), now());
---
+++ mysql/init-roles.sql
@@ -0,0 +1,8 @@
+INSERT INTO roles (name, created_at, updated_at)
+VALUES ('ADMIN', now(), now());
+
+INSERT INTO roles (name, created_at, updated_at)
+VALUES ('USER', now(), now());
+
+INSERT INTO roles (name, created_at, updated_at)
+VALUES ('DEV', now(), now());
