dc69a98ddf29153c9d217d976cccca374ce04c7f
rzldev <amrizalrf@gmail.com>
2024-01-19 14:52:16 +0700

    Docker post run

--- post-run.sh
+++ post-run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 docker exec arlina-minio sh -c "/scripts/init.sh";
-
-docker cp .env.frontend arlina-admin:./.env
 docker exec -it arlina-admin sh -c "./generate-env.sh"
-
-docker cp .env.frontend arlina-user:./.env
 docker exec -it arlina-user sh -c "./generate-env.sh"
